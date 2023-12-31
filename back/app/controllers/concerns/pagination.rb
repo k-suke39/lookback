84848ed9c44155459b18ff45e309c5b84c4f372c
k-suke39 <pz.mozarla@gmail.com>
2023-12-31 11:39:46 +0900

    lint

--- back/app/controllers/concerns/pagination.rb
+++ back/app/controllers/concerns/pagination.rb
@@ -3,8 +3,8 @@ module Pagination
 
   def pagination(records)
     {
-      current_page: records.current_page, 
-      total_pages: records.total_pages, 
+      current_page: records.current_page,
+      total_pages: records.total_pages,
     }
   end
 end
